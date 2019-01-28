f409a903fc2ac5a1787fffa4c516e07de2e575fd
冉晨光 <1107743111@qq.com>
2019-01-28 16:10:24 +0800

    按钮组件修改

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    extends: "standard",
+    parser: "babel-eslint",
+    plugins: ["react"],
+    rules: {
+      "react/jsx-uses-react": [2],
+      "react/jsx-uses-vars": [2],
+      "react/react-in-jsx-scope": [2]
+    }
+  };
+  
